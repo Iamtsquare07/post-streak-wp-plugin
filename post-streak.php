64ab38f51c5e44f80356cd3149f743254c79c43e
Victor Tyavbee <68505976+Iamtsquare07@users.noreply.github.com>
2023-12-11 23:45:06 +0100

    Fixed bugs on date handling

--- post-streak.php
+++ post-streak.php
@@ -7,38 +7,46 @@ Author: Tsquare07
 */
 
 // Hook into the post publishing process
-add_action('publish_post', 'update_post_streak');
-// Hook into the post update process
-add_action('save_post', 'update_post_streak');
+add_action('transition_post_status', 'update_post_streak', 10, 3);
 
+// Hook into the dashboard setup to add the widget
 add_action('wp_dashboard_setup', 'post_streak_dashboard_widget');
 
-function update_post_streak($post_id) {
-    if (wp_is_post_revision($post_id)) {
-        return;
-    }
+// Hook into the script and style setup
+add_action('wp_enqueue_scripts', 'enqueue_post_streak_script');
 
-    $author_id = get_post_field('post_author', $post_id);
-    $streak_key = 'post_streak_' . $author_id;
-
-    // Get the current streak
-    $current_streak = get_user_meta($author_id, $streak_key, true);
-
-    // If the user hasn't posted today, reset the streak
-    $last_post_date = strtotime(get_the_time('Y-m-d', $post_id));
-    $today_date = strtotime(date('Y-m-d'));
-    if ($last_post_date < $today_date) {
-        $current_streak = 0;
-    }
-
-    $current_streak++;
-    update_user_meta($author_id, $streak_key, $current_streak);
-
-    // Add an alert to the post/page editor
-    echo "<script>alert('You have posted $current_streak times this week. Keep it up, " . get_the_author_meta('display_name', $author_id) . "!');</script>";
+function enqueue_post_streak_script() {
+    // Enqueue script for the alert
+    wp_enqueue_script('post-streak-alert', plugin_dir_url(__FILE__) . 'post-streak-alert.js', array(), '1.0', true);
 }
 
-// Function to display the dashboard widget
+function update_post_streak($new_status, $old_status, $post) {
+    if ($new_status === 'publish' && $old_status !== 'publish') {
+        $author_id = $post->post_author;
+        $streak_key = 'post_streak_' . $author_id;
+
+        // Get the current streak
+        $current_streak = get_user_meta($author_id, $streak_key, true);
+
+        // If the user hasn't posted today, reset the streak
+        $last_post_date = strtotime(date('Y-m-d', strtotime($post->post_date)));
+        $today_date = strtotime(date('Y-m-d'));
+
+        if ($last_post_date < $today_date) {
+            $current_streak = 0;
+        }
+
+        $current_streak++;
+        update_user_meta($author_id, $streak_key, $current_streak);
+
+        // Set a message
+        $message = 'Congratulations! You have posted again. Keep up the good work!';
+
+        // Pass data to the script
+        wp_localize_script('post-streak-alert', 'postStreakData', array('message' => $message));
+    }
+}
+
 function post_streak_dashboard_widget() {
     wp_add_dashboard_widget(
         'post_streak_dashboard_widget',
@@ -47,18 +55,19 @@ function post_streak_dashboard_widget() {
     );
 }
 
-// Function to display the content of the dashboard widget
+// Display the content of the dashboard widget
 function display_post_streak() {
     $user_id = get_current_user_id();
     $streak_key = 'post_streak_' . $user_id;
     $current_streak = get_user_meta($user_id, $streak_key, true);
 
-    echo '<p>';
+    echo '<div style="padding: 20px; background-color: #f4f4f4; border: 1px solid #ddd; border-radius: 5px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">';
     if ($current_streak > 0) {
-        echo 'You have posted ' . $current_streak . ' times this week. Keep it up, ' . get_the_author_meta('display_name', $user_id) . '!';
+        echo '<p style="font-size: 16px; font-weight: bold; color: #333;">You have posted ' . $current_streak . ' times this week. Keep it up, ' . get_the_author_meta('display_name', $user_id) . '!</p>';
     } else {
-        echo 'Start your posting streak today!';
+        echo '<p style="font-size: 16px; font-weight: bold; color: #333;">Start your posting streak today!</p>';
     }
-    echo '</p>';
+    echo '</div>';
 }
+
 ?>
